01c248686becabea605d5de53ce7c77d90530ed1
malikmoneyT <95098139+malikmoneyT@users.noreply.github.com>
2024-07-01 23:12:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,21 @@
-from flask import Flask, request, jsonify, render_template
-import boto3
-from templates import *
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
+from flask import Flask, render_template, request, jsonify
+from comprehend_service import ComprehendService
 
 app = Flask(__name__)
 
-# Initialize the Amazon Comprehend client
-comprehend = boto3.client('comprehend', region_name='ca-central-1')
-
-
+AWS_REGION = 'ca-central-1'
+comprehend_service = ComprehendService(AWS_REGION)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 @app.route('/analyze', methods=['POST'])
 def analyze():
-    text = request.json.get('text')
+    text = request.form['text']
     if not text:
         return jsonify({'error': 'No text provided'}), 400
-
-    # Call Amazon Comprehend to detect sentiment
-    response = comprehend.detect_sentiment(Text=text, LanguageCode='en')
-    sentiment_scores = response['SentimentScore']
-    return jsonify(sentiment_scores)
-
+    result = comprehend_service.analyze_sentiment(text)
+    sentiment = result['Sentiment']
+    return jsonify(sentiment)
 
 if __name__ == '__main__':
     app.run(debug=True)
